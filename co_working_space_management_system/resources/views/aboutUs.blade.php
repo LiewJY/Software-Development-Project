a85b755eae5ed0cce29f5d44f3775b948e017282
WengHoe2001 <wenghoe2001cincin@gmail.com>
2020-12-08 15:00:47 +0800

    abt_us

--- co_working_space_management_system/resources/views/aboutUs.blade.php
+++ co_working_space_management_system/resources/views/aboutUs.blade.php
@@ -117,12 +117,15 @@
       </div>
     </div>
   </div>
+</section>
+
 <!-- Why Us? -->
-  <div class="container px-5 py-24 mx-auto">
+<section class="text-gray-700 body-font overflow-hidden bg-gray-200">
+<div class="container px-5 py-24 mx-auto">
   <div class="flex flex-col text-center w-full mb-12">
       <h1 class="font-serif sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 underline">WHY US?</h1>
-    </div>
-    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+  </div>
+    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-300 sm:flex-row flex-col">
       <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
           <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
@@ -138,7 +141,7 @@
         </a>
       </div>
     </div>
-    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-300 sm:flex-row flex-col">
       <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Powered by ShareSpace</h2>
         <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
