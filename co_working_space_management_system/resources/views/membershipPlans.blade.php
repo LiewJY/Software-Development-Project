24f24188049657aef0b73dcbb308adeef43bbf4a
NoobPatata <49740113+NoobPatata@users.noreply.github.com>
2020-12-09 21:39:33 +0800

    Update membershipPlans.blade.php

--- co_working_space_management_system/resources/views/membershipPlans.blade.php
+++ co_working_space_management_system/resources/views/membershipPlans.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.page')
 
 @section('content')
-    <h1 class="text-center font-bold text-5xl mt-8 ">Avaliable Plans</h1>
+    <h1 class="text-center font-bold text-5xl mt-8 ">Available Plans</h1>
     <div class="grid md:grid-cols-3 gap-4">
         @livewire('membership-plans')
 
