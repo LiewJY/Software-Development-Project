ce0ead212cd9390942955aafb2c138664732ce6b
WengHoe2001 <wenghoe2001cincin@gmail.com>
2020-12-17 14:01:52 +0800

    abt_us

--- co_working_space_management_system/resources/views/aboutUs.blade.php
+++ co_working_space_management_system/resources/views/aboutUs.blade.php
@@ -4,30 +4,30 @@
 <!-- Hero -->
 <header class="relative bg-no-repeat bg-cover lg:bg-center p-20" style="background-image: url('https://fj-employer-blog.s3.amazonaws.com/employer-blog/wp-content/uploads/2016/12/30043608/The-Benefits-of-Coworking-Spaces-for-Corporations.jpg')">
 <div class="max-w-5xl mx-auto lg:py-32 py-2">
-    <h2 class="lg:text-5xl text-4xl font-semibold text-gray-700 leading-none mb-4">
+    <h2 class="lg:text-5xl text-4xl font-bold text-gray-700 leading-none mb-4">
     Welcome to ShareSpace
     </h2>
 <div class="flex items-center flex-wrap justify-start max-w-2xl lg:mx-0 mx-auto">
     <div class="lg:pr-2 w-full lg:w-1/2 lg:mb-0">
-        <a href="#" class="transition bg-gray-100 px-10 py-3 rounded font-bold hover:bg-gray-300 block w-full text-center border-2 border-white">Explore</a>
+        <a href="#" class="transition bg-gray-100 px-10 py-3 rounded font-bold hover:bg-gray-300 block w-full text-center border-2 border-white">Explore!</a>
     </div>
     <div class="lg:pl-5 w-full lg:w-1/2">
-        <a href="#" class="transition bg-transparent px-10 py-3 rounded font-bold hover:bg-gray-800 hover:text-gray-100 text-black block w-full text-center border-2 border-gray-900">Book a Tour!</a>
+        <a href="#" class="transition bg-transparent px-10 py-3 rounded font-bold hover:bg-gray-800 hover:text-gray-100 text-black block w-full text-center border-2 border-gray-900">View Membership Plans!</a>
     </div>
 </div>
 </div>
 </header>
 
-<section class="text-gray-700 body-font overflow-hidden bg-gray-100">
+<section class="text-gray-700 body-font overflow-hidden bg-gray-200">
 <!-- Title -->
 <div class="container px-5 py-15 mx-auto flex flex-col">
     <div class="lg:w-4/6 mx-auto">
       <div class="flex flex-col text-center w-full mb-1">
-      <h1 class="font-serif sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 underline">ABOUT US</h1>
+      <h1 class="font-serif sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 underline animate-bounce">ABOUT US</h1>
       </div>
 
 <!-- Content Founder -->
-      <div class="flex flex-col sm:flex-row mt-10">
+      <div class="flex flex-col sm:flex-row mt-10 py-3 px-3 border border-gray-300 rounded-lg bg-gray-100">
         <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
           <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
@@ -36,27 +36,22 @@
             </svg>
           </div>
           <div class="flex flex-col items-center text-center justify-center">
-            <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">Phoebe Caulfield</h2>
+            <h2 class="font-medium title-font mt-4 text-gray-900 text-lg font-bold">Phoebe Caulfield</h2>
             <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
             <p class="text-base text-gray-600">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
           </div>
         </div>
         <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-300 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
           <p class="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
-          <a class="text-indigo-500 inline-flex items-center">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
         </div>
       </div>
     </div>
 <!-- Content Staff -->
-    <div class="flex flex-wrap -m-12">
-      <div class="p-12 md:w-1/2 flex flex-col items-start">
-        <span class="inline-block py-1 px-3 rounded bg-indigo-100 text-indigo-500 text-sm font-medium tracking-widest">STAFF</span>
-        <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
-        <p class="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug you probably haven't heard of them hexagon kickstarter craft beer pork chic.</p>
+    <div class="flex flex-wrap -m-12 my-5">
+      <div class="p-12 md:w-1/2 flex flex-col items-start border border-gray-300 bg-gray-100 rounded-lg">
+        <span class="inline-block py-1 px-3 rounded-full bg-indigo-100 text-indigo-500 text-sm font-medium tracking-widest">STAFF</span>
+        <h2 class="sm:text-3xl text-2xl title-font font-bold text-gray-900 mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
+        <p class="leading-relaxed mb-8 font-medium">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug you probably haven't heard of them hexagon kickstarter craft beer pork chic.</p>
         <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-200 mt-auto w-full">
           <a class="text-indigo-500 inline-flex items-center">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -73,21 +68,21 @@
           <span class="text-gray-600 inline-flex items-center leading-none text-sm">
             <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
               <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-            </svg>6
+            </svg>
           </span>
         </div>
         <a class="inline-flex items-center">
           <img alt="blog" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
           <span class="flex-grow flex flex-col pl-4">
-            <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
+            <span class="title-font font-bold text-gray-900">Holden Caulfield</span>
             <span class="text-gray-500 text-sm">UI DEVELOPER</span>
           </span>
         </a>
       </div>
-      <div class="p-12 md:w-1/2 flex flex-col items-start">
-        <span class="inline-block py-1 px-3 rounded bg-indigo-100 text-indigo-500 text-sm font-medium tracking-widest">STAFF</span>
-        <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Pinterest DIY dreamcatcher gentrify single-origin coffee</h2>
-        <p class="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric.</p>
+      <div class="p-12 md:w-1/2 flex flex-col items-start border border-gray-300 bg-gray-100 rounded-lg">
+        <span class="inline-block py-1 px-3 rounded-full bg-indigo-100 text-indigo-500 text-sm font-medium tracking-widest">STAFF</span>
+        <h2 class="sm:text-3xl text-2xl title-font font-bold text-gray-900 mt-4 mb-4">Pinterest DIY dreamcatcher gentrify single-origin coffee</h2>
+        <p class="leading-relaxed mb-8 font-medium">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric.</p>
         <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-200 mt-auto w-full">
           <a class="text-indigo-500 inline-flex items-center">Learn More
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -104,13 +99,14 @@
           <span class="text-gray-600 inline-flex items-center leading-none text-sm">
             <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
               <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-            </svg>6
+            </svg>
           </span>
         </div>
         <a class="inline-flex items-center">
           <img alt="blog" src="https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+          
           <span class="flex-grow flex flex-col pl-4">
-            <span class="title-font font-medium text-gray-900">Alper Kamu</span>
+            <span class="title-font font-bold text-gray-900">Alper Kamu</span>
             <span class="text-gray-500 text-sm">DESIGNER</span>
           </span>
         </a>
@@ -120,60 +116,50 @@
 </section>
 
 <!-- Why Us? -->
-<section class="text-gray-700 body-font overflow-hidden bg-gray-200">
-<div class="container px-5 py-24 mx-auto">
-  <div class="flex flex-col text-center w-full mb-12">
-      <h1 class="font-serif sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 underline">WHY US?</h1>
+<section class="text-gray-700 body-font bg-gray-500">
+  <div class="flex flex-col text-center w-full pt-10">
+      <h1 class="font-serif sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 underline animate-bounce">WHY CHOOSE US?</h1>
   </div>
-    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-300 sm:flex-row flex-col">
-      <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-          <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-        </svg>
-      </div>
-      <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-        <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Strategic Locations</h2>
-        <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-        <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
+  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden border border-gray-300">
+      <img alt="feature" class="object-cover object-center h-full w-full" src="https://image.shutterstock.com/image-photo/why-choose-260nw-612482153.jpg">
     </div>
-    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-300 sm:flex-row flex-col">
-      <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-        <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Powered by ShareSpace</h2>
-        <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-        <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
+    <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+      <div class="flex flex-col mb-10 lg:items-start items-center border rounded-lg py-5 px-5 bg-gray-200">
+        <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 border">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
           </svg>
-        </a>
+        </div>
+        <div class="flex-grow">
+          <h2 class="text-gray-900 text-lg title-font font-bold mb-3">Shooting Stars</h2>
+          <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+        </div>
       </div>
-      <div class="sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-          <circle cx="6" cy="6" r="3"></circle>
-          <circle cx="6" cy="18" r="3"></circle>
-          <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-        </svg>
-      </div>
-    </div>
-    <div class="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
-      <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-          <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-          <circle cx="12" cy="7" r="4"></circle>
-        </svg>
-      </div>
-      <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-        <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Workspace as a service</h2>
-        <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-        <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
+      <div class="flex flex-col mb-10 lg:items-start items-center border rounded-lg py-5 px-5 bg-gray-200">
+        <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 border">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+            <circle cx="6" cy="6" r="3"></circle>
+            <circle cx="6" cy="18" r="3"></circle>
+            <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
           </svg>
-        </a>
+        </div>
+        <div class="flex-grow">
+          <h2 class="text-gray-900 text-lg title-font font-bold mb-3">The Catalyzer</h2>
+          <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+        </div>
+      </div>
+      <div class="flex flex-col mb-10 lg:items-start items-center border rounded-lg py-5 px-5 bg-gray-200">
+        <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 border">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </div>
+        <div class="flex-grow">
+          <h2 class="text-gray-900 text-lg title-font font-bold mb-3">Neptune</h2>
+          <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+        </div>
       </div>
     </div>
   </div>
