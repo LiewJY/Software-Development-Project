bc9f57f54b7929f18bac4395b0c7918f96cddb6d
NoobPatata <noobman2613@gmail.com>
2020-12-19 16:49:17 +0800

    Created CRUD for location in admin

--- co_working_space_management_system/app/Http/Livewire/Admin/LocationTable.php
+++ co_working_space_management_system/app/Http/Livewire/Admin/LocationTable.php
@@ -4,23 +4,81 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use Livewire\WithPagination;
-
 use App\Models\Location;
-
 
 class LocationTable extends Component
 {
-
-    public $search='';
+    use WithPagination;
+    public $name, $address, $contactNumber, $description, $locationID;
+    public $search = '';
     protected $queryString = ['search'];
+
+    /**
+     * Validation rules that are applied to location 
+     *
+     * @var array
+     */
+    protected $rules = [
+        'name' => 'required',
+        'address' => 'required|max:255',
+        'contactNumber' => 'required|regex:/^(01)[0-46-9]*[0-9]{7,8}$/',
+        'description' => 'required|max:255'
+    ];
 
 
     public function render()
     {
-        return view('livewire.admin.location-table',[
-            'locations' => location::where('name', 'like', '%'.$this->search.'%')->paginate(25),
+        return view('livewire.admin.location-table', [
+            'locations' => location::where('name', 'like', '%' . $this->search . '%')->paginate(25),
+        ]);
+    }
+
+    /**
+     * Create or update location 
+     *
+     * @return void
+     */
+    public function store()
+    {
+        $this->validate();
+
+        Location::updateOrCreate(['id' => $this->locationID], [
+            'name' => $this->name,
+            'address' => $this->address,
+            'contact_number' => $this->contactNumber,
+            'description' =>  $this->description
         ]);
 
+        session()->flash(
+            'message',
+            $this->location_id ? 'Location Updated Successfully.' : 'Location Created Successfully.'
+        );
+    }
 
+    /**
+     * Pass location id and it will fill the feilds with data from database
+     *
+     * @param  int $id
+     * @return void
+     */
+    public function edit($id)
+    {
+        $location = Location::findOrFail($id);
+        $this->locationID = $id;
+        $this->name = $location->name;
+        $this->address = $location->address;
+        $this->contactNumber = $location->contact_number;
+        $this->description = $location->description;
+    }
+
+    /**
+     * Delete selected location
+     *
+     * @param  int $id
+     * @return void
+     */
+    public function delete($id)
+    {
+        Location::where('id', $id)->delete();
     }
 }
