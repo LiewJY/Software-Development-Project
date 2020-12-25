763b9126aff1ccc9ed0f8b0b6b855ae9078d83b7
WengHoe2001 <wenghoe2001cincin@gmail.com>
2020-12-25 20:50:23 +0800

    businessReport&staff

--- co_working_space_management_system/app/Http/Livewire/Admin/Staff.php
+++ co_working_space_management_system/app/Http/Livewire/Admin/Staff.php
@@ -2,29 +2,36 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Employee;
+use App\Models\User;
 
 class Staff extends Component
 {
     use WithPagination;
+    use PasswordValidationRules;
     public $employeeForm = false;
+    public $employeeAddForm = false;
     public $deleteConEmployeeForm = false;
-    public $first_name, $last_name, $address, $contactNumber, $employeeID;
+    public $username, $email, $password, $roles, $firstName, $lastName, $address, $contactNumber, $employeeID;
     public $search = '';
     protected $queryString = ['search'];
 
     /**
-     * Validation rules that are applied to location 
+     * Employee creation validation rules
      *
      * @var array
      */
     protected $rules = [
-        'first_name' => 'required',
-        'last_name' => 'required',
-        'address' => 'required|max:255',
-        'contactNumber' => 'required|regex:/^(01)[0-46-9]*[0-9]{7,8}$/',
+        'username' => 'required' | 'string' | 'min:6' | 'max:255' | 'unique:users',
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'roles' => ['required'],
+        'firstName' => ['required', 'string', 'max:255'],
+        'lastName' => ['required', 'string', 'max:255'],
+        'address' => ['required', 'string', 'max:255'],
+        'contactNumber' => ['required', 'regex:/^(01)[0-46-9]*[0-9]{7,8}$/']
     ];
 
 
@@ -38,49 +45,58 @@ class Staff extends Component
     public function add()
     {
         $this->reset();
-        $this->employeeForm = true;
+        $this->employeeAddForm = true;
         
     }
 
-    /**
-     * Create or update location 
+   /**
+     * Creating or updating existing employee data
      *
      * @return void
      */
     public function store()
     {
-        $this->validate();
-
-        Employee::updateOrCreate(['id' => $this->employeeID], [
-            'first_name' => $this->first_name,
-            'last_name' =>  $this->last_name,
-            'address' => $this->address,
-            'contact_number' => $this->contactNumber
+        $this->validate([
+            'password'  => $this->passwordRules()
         ]);
-        $this->employeeForm = false;
 
+        $userID = Employee::where('id', $this->employeeID)->first();
 
-        session()->flash(
-            'message',
-            $this->employeeID ? 'Location Updated Successfully.' : 'Location Created Successfully.'
-        );
+        if ($userID) {
+            $this->userID = $userID->user_id;
+        }
+
+        $user = User::updateorCreate(['id' => $this->userID], [
+            'username' => $this->username,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+            'roles' => $this->roles
+        ]);
+
+        $employee = Employee::updateorCreate(['user_id' => $this->userID], [
+            'first_name' => $this->firstName,
+            'last_name' => $this->lastName,
+            'address' => $this->address,
+            'contact_number' => $this->contactNumber,
+        ]);
+
+        $user->employee()->save($employee);
     }
 
     /**
-     * Pass location id and it will fill the feilds with data from database
+     * Fill feilds with employee data from database
      *
      * @param  int $id
      * @return void
      */
     public function edit($id)
-    {
+    {   
         $this->employeeForm = true;
-        $employee = Employee::findOrFail($id);
-        $this->employeeID = $id;
-        $this->first_name = $employee->first_name;
-        $this->last_name = $employee->last_name;
+        $employee = Employee::findorFail($id);
+        $this->firstName =  $employee->first_name;
+        $this->lastName = $employee->last_name;
         $this->address = $employee->address;
-        $this->contactNumber = $employee->contact_number;
+        $this->contactNumber =  $employee->contact_number;
     }
 
     public function deleteModal($id, $first_name)
@@ -91,14 +107,14 @@ class Staff extends Component
         
     }
     /**
-     * Delete selected location
+     * Delete selected employee
      *
      * @param  int $id
      * @return void
      */
     public function delete($id)
     {
-        Employee::where('id', $id)->delete();
-        $this->deleteConEmployeeForm = false;
+        $employee =  Employee::where('id', $id)->firstorfail();
+        User::where('id', $employee->user_id)->firstorfail()->delete();
     }
 }
--- co_working_space_management_system/resources/views/livewire/admin/staff.blade.php
+++ co_working_space_management_system/resources/views/livewire/admin/staff.blade.php
@@ -1,4 +1,8 @@
 <div>
+<!-- Title -->
+<h1 class="font-serif sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 underline mx-3">Employee</h1>
+
+<!-- Search -->
 <div class="flex flex-row flex-wrap-reverse justify-between mt-4 px-2 py-2">
     <div class="w-full md:w-1/2">
         <x-jet-input class="w-full" type="search" wire:model="search" placeholder="Search by Name"/>
@@ -7,6 +11,8 @@
         <x-jet-button class="w-full flex items-center justify-center md:w-auto" wire:click="add">Add Employee</x-jet-button>
     </div>
 </div>
+
+<!-- Table -->
 <div class="overflow-x-auto mx-1">
     <table class="min-w-full table-auto border-collapse border border-black">
         <thead>
@@ -28,7 +34,7 @@
                     <td class="border border-black  py-1.5">
                         <div class="border-none flex flex-row flex-nowrap justify-center">
                             <x-jet-button class="mx-2" wire:click="edit({{$employee ->id}})">Edit</x-jet-button>
-                            <x-jet-button class="mx-2" wire:click="deleteModal({{$employee ->id}}, '{{$employee ->first_name}}')">Delete</x-jet-button>
+                            <x-jet-button class="mx-2" wire:click="deleteModal({{$employee ->id}}, '{{$employee ->firstName}}')">Delete</x-jet-button>
                         </div>
                     </td>
 
@@ -39,52 +45,90 @@
     </table>
     {{$employees->links()}} 
 
+<!-- Edit Modal -->
     <x-jet-dialog-modal wire:model="employeeForm" >
         <x-slot name="title">
-                @if($employeeID)
-                    <h1>Edit Employee</h1>
-                @else
-                    <h1>Add Employee</h1>
-                @endif
+            <h1>Edit Employee</h1>
         </x-slot>
         <form>
             <x-slot name="content">
-                <x-jet-label for="first_name" value="First name"/>
-                <x-jet-input id="first_name" type="text" class="mt-1 block w-full" wire:model.defer="first_name"/>
-                <x-jet-input-error for="first_name"/>
+                <x-jet-label for="firstName" value="First name"/>
+                <x-jet-input id="firstName" type="text" class="mt-1 block w-full" wire:model.lazy="firstName"/>
+                <x-jet-input-error for="firstName"/>
 
-                <x-jet-label for="last_name" value="Last name" />
-                <x-jet-input id="last_name" type="text" class="mt-1 block w-full" wire:model.defer="last_name"/>
-                <x-jet-input-error for="last_name"/>
+                <x-jet-label for="lastName" value="Last name" />
+                <x-jet-input id="lastName" type="text" class="mt-1 block w-full" wire:model.lazy="lastName"/>
+                <x-jet-input-error for="lastName"/>
 
                 <x-jet-label for="address" value="Address"/>
-                <x-jet-input id="address" type="text" class="mt-1 block w-full" wire:model.defer="address"/>
+                <x-jet-input id="address" type="text" class="mt-1 block w-full" wire:model.lazy="address"/>
                 <x-jet-input-error for="address"/>
 
                 <x-jet-label for="contactNumber" value="Contact Number" />
-                <x-jet-input id="contactNumber" type="text" class="mt-1 block w-full" wire:model.defer="contactNumber"/>
+                <x-jet-input id="contactNumber" type="text" class="mt-1 block w-full" wire:model.lazy="contactNumber"/>
                 <x-jet-input-error for="contactNumber"/>
             </x-slot>
             <x-slot name="footer">
-                @if($employeeID)
-                    <x-jet-button wire:click="store">Save</x-jet-button>
-                @else
-                    <x-jet-button wire:click="store">Add</x-jet-button>
-                @endif
-                    <x-jet-button wire:click="$toggle('employeeForm')">Cancel</x-jet-button>
-
-
+                <x-jet-button wire:click="store">Save</x-jet-button>
+                <x-jet-button wire:click="$toggle('employeeForm')">Cancel</x-jet-button>
             </x-slot>
         </form>
     </x-jet-dialog-modal>
 
+<!-- Add Modal -->
+    <x-jet-dialog-modal wire:model="employeeAddForm" >
+        <x-slot name="title">
+            <h1>Add Employee</h1>
+        </x-slot>
+    <form>
+            <x-slot name="content">
+                <div class="flex justify-between gap-3">
+                <span class="w-1/2">
+                    <x-jet-label for="firstName" value="First name"/>
+                    <x-jet-input id="firstName" type="text" class="mt-1 block w-full" wire:model.lazy="firstName"/>
+                    <x-jet-input-error for="firstName"/>
+                </span>
+                <span class="w-1/2">
+                    <x-jet-label for="lastName" value="Last name" />
+                    <x-jet-input id="lastName" type="text" class="mt-1 block w-full" wire:model.lazy="lastName"/>
+                    <x-jet-input-error for="lastName"/>
+                </span>
+                </div>
+                <x-jet-label for="address" value="Address"/>
+                <x-jet-input id="address" type="text" class="mt-1 block w-full" wire:model.lazy="address"/>
+                <x-jet-input-error for="address"/>
+
+                <x-jet-label for="contactNumber" value="Contact Number" />
+                <x-jet-input id="contactNumber" type="text" class="mt-1 block w-full" wire:model.lazy="contactNumber"/>
+                <x-jet-input-error for="contactNumber"/>
+
+                <x-jet-label for="username" value="Username" />
+                <x-jet-input id="usernamer" type="text" class="mt-1 block w-full" wire:model.lazy="username"/>
+                <x-jet-input-error for="username"/>
+
+                <x-jet-label for="email" value="Email" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full" wire:model.lazy="email"/>
+                <x-jet-input-error for="email"/>
+
+                <x-jet-label for="password" value="Password" />
+                <x-jet-input id="password" type="text" class="mt-1 block w-full" wire:model.lazy="password"/>
+                <x-jet-input-error for="password"/>
+            </x-slot>
+            <x-slot name="footer">
+                <x-jet-button wire:click="edit">Add</x-jet-button>
+                <x-jet-button wire:click="$toggle('employeeAddForm')">Cancel</x-jet-button>
+            </x-slot>
+        </form>
+    </x-jet-dialog-modal>    
+
+<!-- Delete Modal -->
     <x-jet-dialog-modal wire:model="deleteConEmployeeForm">
         <x-slot name="title">
             <h1>Delete Confirmation</h1>
         </x-slot>
         <form>
             <x-slot name="content">
-                <p>Are you sure you want to remove the employee with the name {{$first_name}}</p>
+                <p>Are you sure you want to remove the employee with the name {{$firstName}}</p>
             </x-slot>
             <x-slot name="footer">
                 <x-jet-button wire:click="delete({{$employeeID}})">Delete</x-jet-button>
