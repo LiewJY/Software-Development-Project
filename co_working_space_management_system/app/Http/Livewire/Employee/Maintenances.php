0c328eecb438bcf9bd9904606d62e23b76ac5f9a
Liew Jun Young <liewjunyoung@gmail.com>
2021-01-02 22:59:26 +0800

    change file name maintenance to maintenances

---
+++ co_working_space_management_system/app/Http/Livewire/Employee/Maintenances.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire\Employee;
+
+use Livewire\Component;
+
+class Maintenances extends Component
+{
+    public function render()
+    {
+        return view('livewire.employee.maintenances');
+    }
+
+
+
+}
