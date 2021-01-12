191e711d05f20ee04ef45b591131606d8d177314
WengHoe2001 <wenghoe2001cincin@gmail.com>
2021-01-12 22:59:10 +0800

    added file for customer view

---
+++ co_working_space_management_system/app/Http/Livewire/LocationRooms.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LocationRooms extends Component
+{
+    public function render()
+    {
+        return view('livewire.location-rooms');
+    }
+}
