49ac421673ddbafb2b3de19c78943c713d8e3349
Liew Jun Young <liewjunyoung@gmail.com>
2021-01-30 00:57:26 +0800

    added subscriptionn history

--- co_working_space_management_system/resources/views/livewire/customer/subscription-history.blade.php
+++ co_working_space_management_system/resources/views/livewire/customer/subscription-history.blade.php
@@ -2,7 +2,7 @@
     <div>
     <div class="flex flex-row flex-wrap-reverse justify-between mt-4 px-2 py-2">
         <div class="w-full md:w-1/2">
-            <p class="w-full">Subscription history</p>
+            <h1 class="px-2 font-bold text-xl md:text-2xl pt-2">Subscription History</h1>
             {{-- <x-jet-input class="w-full" type="search" wire:model="search" placeholder="Search by Location"/> --}}
         </div>
         <div class="w-full flex md:justify-end md:w-1/2 mb-3 md:mb-0">
@@ -11,43 +11,49 @@
     </div>
     <br>
     <div class="overflow-x-auto mx-1">
-        <table class="min-w-full table-auto border-collapse border border-black">
-            <thead>
-                <tr>
-                    <th class="border border-gray-700 text-white bg-gray-700">Plan Name</th>
-                    <th class="border border-gray-700 text-white bg-gray-700">Start Date</th>
-                    <th class="border border-gray-700 text-white bg-gray-700">End Date</th>
-                    <th class="border border-gray-700 text-white bg-gray-700">Price</th>
-                    <th class="border border-gray-700 text-white bg-gray-700">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-            @if(count($subscriptions) === 0 )
-                you do not have any subscription (will style later)
-            @else
-
-                @foreach ($subscriptions as $subscription)
-                    <tr >
-                        <td class="border border-gray-400 bg-gray-100">{{$subscription->membership->name}}</td>
-                        @php
-                            $start_at = date('Y-m-d', strtotime($subscription->updated_at));
-                            $end_at = date('Y-m-d', strtotime($subscription->updated_at->addDays(30)));
-                        @endphp
-                        <td class="border border-gray-400 bg-gray-100">{{$start_at}}</td>
-                        <td class="border border-gray-400 bg-gray-100">{{$end_at}}</td>
-                        <td class="border border-gray-400 bg-gray-100">RM {{$subscription->membership->price}}</td>
-                        <td class="border border-gray-400 bg-gray-100">
-                            <div class="border-none flex flex-row flex-nowrap justify-center">
-                                <x-jet-button class="mx-2" wire:click="print({{$subscription->id}})">Print Receipt</x-jet-button>
-                            </div>
-                        </td>
-                     </tr>               
-                @endforeach
-            </tbody>
-            @endif
-        </table>
-        <br>
-        {{$subscriptions->links()}}
+        @if(count($subscriptions) === 0 )
+                <x-emptyTable>
+                <x-slot name="header">
+                    Subscription History
+                </x-slot>
+                <x-slot name="content">
+                    Looks like you do not have any subscription
+                </x-slot>
+            </x-emptyTable>
+        @else
+            <table class="min-w-full table-auto border-collapse border border-black">
+                <thead>
+                    <tr>
+                        <th class="border border-gray-700 text-white bg-gray-700">Plan Name</th>
+                        <th class="border border-gray-700 text-white bg-gray-700">Start Date</th>
+                        <th class="border border-gray-700 text-white bg-gray-700">End Date</th>
+                        <th class="border border-gray-700 text-white bg-gray-700">Price</th>
+                        <th class="border border-gray-700 text-white bg-gray-700">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($subscriptions as $subscription)
+                        <tr >
+                            <td class="border border-gray-400 bg-gray-100">{{$subscription->membership->name}}</td>
+                            @php
+                                $start_at = date('Y-m-d', strtotime($subscription->updated_at));
+                                $end_at = date('Y-m-d', strtotime($subscription->updated_at->addDays(30)));
+                            @endphp
+                            <td class="border border-gray-400 bg-gray-100">{{$start_at}}</td>
+                            <td class="border border-gray-400 bg-gray-100">{{$end_at}}</td>
+                            <td class="border border-gray-400 bg-gray-100">RM {{$subscription->membership->price}}</td>
+                            <td class="border border-gray-400 bg-gray-100">
+                                <div class="border-none flex flex-row flex-nowrap justify-center">
+                                    <x-jet-button class="mx-2" wire:click="print({{$subscription->id}})">Print Receipt</x-jet-button>
+                                </div>
+                            </td>
+                        </tr>               
+                    @endforeach
+                </tbody>
+            </table>
+            <br>
+            {{$subscriptions->links()}}
+        @endif
     </div>
 </div>
 
