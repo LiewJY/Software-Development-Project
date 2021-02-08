e5155d0c08b8b04b35b1d55bc669ead4a9965777
NoobPatata <49740113+NoobPatata@users.noreply.github.com>
2021-02-08 10:51:53 +0800

    fixed flash message fade out

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -17,11 +17,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
-    
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -492,13 +492,11 @@
 @livewireScripts
 @livewireChartsScripts
 <script>
-    document.addEventListener("livewire:load", function(event) {
-        window.livewire.hook('message.processed', () => {
-            setTimeout(function() {
-                $('#alert').fadeOut('slow');
-            }, 3000);
-        });
-    });
+    $("document").ready(function() {
+        setTimeout(function() {
+            $('#alert').fadeOut('slow');
+        }, 3000)
+    })
 </script>
 
 </html>
